e6a632615c102de21395e4727776c617e4dca909
dlogushiv <dlogushiv@gmail.com>
2016-08-22 13:28:03 +0300

    Changed class name. Repaired method maxPositive. Added new method for search second largest element (bubble sort) and new methods for double types.

---
+++ src/module2/homework2/HomeWork2_1.java
@@ -0,0 +1,165 @@
+package module2.homework2;
+
+public class HomeWork2_1 {
+
+    static int sum(int[] array) {
+        int sum = 0;
+        for (int element : array) {
+            sum += element;
+        }
+        return sum;
+    }
+
+    static int min(int[] array) {
+        int min = array[0];
+        for (int element : array) {
+            if (element <= min) {
+                min = element;
+            }
+        }
+        return min;
+    }
+
+    static int max(int[] array) {
+        int max = array[0];
+        for (int element : array) {
+            if (element >= max) {
+                max = element;
+            }
+        }
+        return max;
+    }
+
+    static int maxPositive(int[] array) {
+        int maxPositive = 0;
+        for (int element : array) {
+            if (element > 0 && element >= maxPositive) {
+                maxPositive = element;
+            }
+        }
+        return maxPositive;
+    }
+
+    static long multiplication(int[] array) {
+        long multiplication = 1;
+        for (int element : array) {
+            multiplication *= element;
+        }
+        return multiplication;
+    }
+
+    static int modulus(int[] array) {
+        int mod = array[0] % array[array.length - 1];
+        return mod;
+    }
+
+    static int secondLargest(int[] array) {
+        boolean ind = true; // indicator of sorting
+        int t;              // temp variable for min between two elements
+
+        while (ind) {
+            ind = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    t = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = t;
+                    ind = true;
+                }
+            }
+        }
+        return array[array.length - 2];
+    }
+
+    static double sum(double[] array) {
+        double sum = 0;
+        for (double element : array) {
+            sum += element;
+        }
+        return sum;
+    }
+
+    static double min(double[] array) {
+        double min = array[0];
+        for (double element : array) {
+            if (element <= min) {
+                min = element;
+            }
+        }
+        return min;
+    }
+
+    static double max(double[] array) {
+        double max = array[0];
+        for (double element : array) {
+            if (element >= max) {
+                max = element;
+            }
+        }
+        return max;
+    }
+
+    static double maxPositive(double[] array) {
+        double maxPositive = 0;
+        for (double element : array) {
+            if (element > 0 && element >= maxPositive) {
+                maxPositive = element;
+            }
+        }
+        return maxPositive;
+    }
+
+    static double multiplication(double[] array) {
+        double multiplication = 1;
+        for (double element : array) {
+            multiplication *= element;
+        }
+        return multiplication;
+    }
+
+    static double modulus(double[] array) {
+        double mod = array[0] % array[array.length - 1];
+        return mod;
+    }
+
+    static double secondLargest(double[] array) {
+        boolean ind = true; // indicator of sorting
+        double t;              // temp variable for min between two elements
+
+        while (ind) {
+            ind = false;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    t = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = t;
+                    ind = true;
+                }
+            }
+        }
+        return array[array.length - 2];
+    }
+
+    public static void main(String[] args) {
+        int[] setOfNumbers = {-30, 50, 10, -90, -120, -70, -5, -1, -15, 8};
+
+        System.out.println("I-1. Sum of the array elements: " + sum(setOfNumbers) + ".");
+        System.out.println("I-2. Minimal element of array: " + min(setOfNumbers) + ".");
+        System.out.println("I-3. Maximal element of array: " + max(setOfNumbers) + ".");
+        System.out.println("I-4. Maximal positive element of array: " + maxPositive(setOfNumbers) + ".");
+        System.out.println("I-5. Multiplication of the array elements: " + multiplication(setOfNumbers) + ".");
+        System.out.println("I-6. Modulus of first and last elements of array: " + modulus(setOfNumbers) + ".");
+        System.out.println("I-7. Second largest element of array: " + secondLargest(setOfNumbers) + ".");
+
+
+        double[] setOfNumbersD = {-30, 50, 10, -90, -120, -70, -5, -1, -15, 8};
+
+        System.out.println("\nD-1. Sum of the array elements: " + sum(setOfNumbersD) + ".");
+        System.out.println("D-2. Minimal element of array: " + min(setOfNumbersD) + ".");
+        System.out.println("D-3. Maximal element of array: " + max(setOfNumbersD) + ".");
+        System.out.println("D-4. Maximal positive element of array: " + maxPositive(setOfNumbersD) + ".");
+        System.out.println("D-5. Multiplication of the array elements: " + multiplication(setOfNumbersD) + ".");
+        System.out.println("D-6. Modulus of first and last elements of array: " + modulus(setOfNumbersD) + ".");
+        System.out.println("D-7. Second largest element of array: " + secondLargest(setOfNumbersD) + ".");
+    }
+}

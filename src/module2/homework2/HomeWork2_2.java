f1c75f2188c8c254b970fe9ff8268cc8039b3508
dlogushiv <dlogushiv@gmail.com>
2016-08-23 10:24:38 +0300

    Completed HomeWork2_2

--- src/module2/homework2/HomeWork2_2.java
+++ src/module2/homework2/HomeWork2_2.java
@@ -3,19 +3,18 @@ package module2.homework2;
 public class HomeWork2_2 {
 
     static double withdrawBalance(double balance, double withdrawal) {
-
-
-        return 0;
+        return (balance - withdrawal - withdrawal * 0.05);
     }
 
     public static void main(String[] args) {
         double balance = 100;
-        double withdraw = 10;
-        double commision = withdraw * 0.05;
+        double withdraw = 96;
 
         double result = withdrawBalance(balance, withdraw);
-        if (result!=-1){
-            System.out.println("OK! Commision: " + commision + " . Balance: " + result);
+        if (result >=0) {
+            System.out.println("Transaction: OK! \nCommission: " + withdraw * 0.05 + ". \nBalance: " + result + ".");
+        } else {
+            System.out.println("Transaction: NO! \nBalance: " + balance + ".");
         }
 
     }

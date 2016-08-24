fc5d60c3ab71d6496c863f080ad13ff48a745078
dlogushiv <dlogushiv@gmail.com>
2016-08-24 10:01:28 +0300

    Updated HomeWork2_3. The data arrays became global at class. Using a method from HomeWork2_2 now.

--- src/module2/homework2/HomeWork2_3.java
+++ src/module2/homework2/HomeWork2_3.java
@@ -1,7 +1,10 @@
 package module2.homework2;
 
 public class HomeWork2_3 {
-    static double withdrawBalance(int[] balances, String[] ownerNames, String ownerName, double withdrawal) {
+    static int[] balances = {1200, 250, 2000, 500, 3200};
+    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+    static double withdrawBalance(String ownerName, double withdrawal) {
         double ownerBalance = 0;
         for (int i = 0; i < ownerNames.length; i++) {
             if (ownerNames[i].equals(ownerName)) {
@@ -9,17 +12,15 @@ public class HomeWork2_3 {
                 break;
             }
         }
-        return (ownerBalance - withdrawal - withdrawal * 0.05);
+//        return (ownerBalance - withdrawal - withdrawal * 0.05);
+        return HomeWork2_2.withdrawBalance(ownerBalance,withdrawal);
     }
 
     public static void main(String[] args) {
-        int[] balances = {1200, 250, 2000, 500, 3200};
-        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
-
         String ownerName = "Jane";
         double withdraw = 240;
 
-        double result = withdrawBalance(balances, ownerNames, ownerName, withdraw);
+        double result = withdrawBalance(ownerName, withdraw);
         if (result >= 0) {
             System.out.println(ownerName + "\nTransaction: OK! \nWithdraw: " + withdraw + ". \nCommission: " + withdraw * 0.05 + ". \nBalance: " + result + ".");
         } else {

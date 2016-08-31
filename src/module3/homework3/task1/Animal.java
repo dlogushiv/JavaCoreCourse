7f46cd0b1b2f3e403728c488ef191c42cc9b288c
dlogushiv <dlogushiv@gmail.com>
2016-08-31 13:57:58 +0300

    Updated HomeWork3_1. Without "static" on classes Animal and Bird

--- src/module3/homework3/task1/Animal.java
+++ src/module3/homework3/task1/Animal.java
@@ -2,7 +2,7 @@ package module3.homework3.task1;
 
 public class Animal {
 
-    public static void walk() {
+    public void walk() {
         System.out.println("I am walking");
     }
 
--- src/module3/homework3/task1/Bird.java
+++ src/module3/homework3/task1/Bird.java
@@ -1,13 +1,15 @@
 package module3.homework3.task1;
 
 public class Bird extends Animal {
-    public static void fly() {
+    public void fly() {
         System.out.println("I am flying");
     }
-    public static void sing() {
+
+    public void sing() {
         System.out.println("I am singing");
     }
-    public static void hello() {
+
+    public void hello() {
         System.out.println("I am Bird");
     }
 }

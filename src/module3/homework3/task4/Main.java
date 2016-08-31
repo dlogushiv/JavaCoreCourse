53d85fcccca4b51f8d137906589c5937c26fd485
dlogushiv <dlogushiv@gmail.com>
2016-08-31 23:58:21 +0300

    HomeWork3_4. Not complete. Only for activate Module 4

---
+++ src/module3/homework3/task4/Main.java
@@ -0,0 +1,9 @@
+package module3.homework3.task4;
+
+public class Main {
+    public static void main(String[] args) {
+//        User user1 = new User();
+//        user1.setBalance(300);
+//        User.paySalary(user1, 200);
+    }
+}
